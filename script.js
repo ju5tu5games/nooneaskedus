d3f312c5bc734f61dfa0c46d468f929049500d94
ju5tu5games <mrbenjaminwatkins@icloud.com>
2025-02-23 20:29:39 +0000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+function openProject() {
+    document.getElementById('notepad-window').classList.remove('hidden');
+}
+
+function closeProject() {
+    document.getElementById('notepad-window').classList.add('hidden');
+}
