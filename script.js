3258179c5a79d18ddaf408e7e67922688c4d919b
ju5tu5games <mrbenjaminwatkins@icloud.com>
2025-02-23 21:32:01 +0000

    Add files via upload

--- script.js
+++ script.js
@@ -1,7 +1,28 @@
+let currentSlide = 0;
+const slides = document.querySelectorAll('.slide');
+
 function openProject() {
     document.getElementById('notepad-window').classList.remove('hidden');
+    showSlide(0);
 }
 
 function closeProject() {
     document.getElementById('notepad-window').classList.add('hidden');
+}
+
+function showSlide(index) {
+    slides.forEach((slide, i) => {
+        slide.classList.remove('active');
+        if (i === index) slide.classList.add('active');
+    });
+}
+
+function nextSlide() {
+    currentSlide = (currentSlide + 1) % slides.length;
+    showSlide(currentSlide);
+}
+
+function prevSlide() {
+    currentSlide = (currentSlide - 1 + slides.length) % slides.length;
+    showSlide(currentSlide);
 }
